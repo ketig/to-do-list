c46f2d061f802a4bfbe10ed665f988c798ab77bb
ketig <41895005+ketig@users.noreply.github.com>
2025-09-23 08:03:11 +0400

    ....

--- script.js
+++ script.js
@@ -17,15 +17,9 @@ taskForm.addEventListener("submit", function (e) {
   // button
   const deleteBtn = document.createElement("button");
   deleteBtn.textContent = "Delete";
-  // deleteBtn.addEventListener("click", function () {
-  //   taskList.removeChild(li);
-  // });
 
   const doneBtn = document.createElement("button");
   doneBtn.textContent = "Done";
-  // doneBtn.addEventListener("click", function () {
-  //   span.style.textDecoration = "line-through";
-  // });
 
   li.appendChild(span);
   li.appendChild(doneBtn);
